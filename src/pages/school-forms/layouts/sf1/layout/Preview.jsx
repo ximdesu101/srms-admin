b3756c49d9bf8a1b416195d746eecae057cf0ece
japheth Dy <otaku.nerdy@gmail.com>
2026-09-17 20:26:57 +0800

    fix preview sheet

--- src/pages/school-forms/layouts/sf1/layout/Preview.jsx
+++ src/pages/school-forms/layouts/sf1/layout/Preview.jsx
@@ -9,31 +9,56 @@ import {
     SheetTitle,
     SheetTrigger,
 } from "@/components/ui/sheet";
-import { Download, Eye } from "lucide-react";
+import {
+    Empty,
+    EmptyContent,
+    EmptyDescription,
+    EmptyHeader,
+    EmptyMedia,
+    EmptyTitle,
+} from "@/components/ui/empty"
+import { Download, Eye, FileText } from "lucide-react";
+
 const Preview = () => {
     return (
         <Sheet>
             <SheetTrigger asChild>
                 <Button variant="outline">
-                    <Eye/>
+                    <Eye />
                     Preview
                 </Button>
             </SheetTrigger>
-            <SheetContent>
+
+            <SheetContent className="sm:max-w-xl">
                 <SheetHeader>
                     <SheetTitle>SF1 - School Register Templates</SheetTitle>
                     <SheetDescription>
                         School Year 2026-2027 • Updated September 2026
                     </SheetDescription>
                 </SheetHeader>
+                <Empty>
+                    <EmptyHeader>
+                        <EmptyMedia variant="icon">
+                            <FileText />
+                        </EmptyMedia>
+                        <EmptyTitle>
+                            Document Preview
+                        </EmptyTitle>
+                        <EmptyDescription>
+                            The official SF1 School Register template
+                            preview will be displayed here.
+                        </EmptyDescription>
+                    </EmptyHeader>
+                </Empty>
                 <SheetFooter className="flex gap-2">
                     <SheetClose asChild>
                         <Button variant="outline">
                             Close Preview
                         </Button>
                     </SheetClose>
+
                     <Button variant="default">
-                        <Download/>
+                        <Download />
                         Download Templates
                     </Button>
                 </SheetFooter>
